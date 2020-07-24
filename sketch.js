47ba99a91efdc42ffd8d2fe8c15e1aaa602cf17a
Chris Berger <cberger4@users.noreply.github.com>
2020-07-24 12:52:40 -0400

    added reset() feature to game
    player can hit SPACE to reset game after they have lost

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//JavaScript Flappy Bird 2.0
+//JavaScript Flappy Bird Extreme 2.0
 //Created with p5.js Web Editor
 //Creator: Chris Berger
 //Created on April 8, 2020
@@ -108,5 +108,4 @@ function reset() {
   score = 0;
   bird = new Bird();
   loop();
-
 }
